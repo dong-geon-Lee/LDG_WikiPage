3bcb570ac402b9d4214d651aaab47d7f3b96ccb2
dong-geon-Lee <ch9716@naver.com>
2023-04-06 11:03:42 +0900

    스타일드 컴포넌트 글로벌 스타일 적용하기

---
+++ src/styles/styles.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0; 
+    padding:0; 
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+    overflow-x: hidden;
+  }
+
+  body {
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    background-color: #f3f3f3;
+    line-height: 1.5;
+    overflow: hidden;
+    position: relative;
+  }
+`;
+
+export default GlobalStyle;
