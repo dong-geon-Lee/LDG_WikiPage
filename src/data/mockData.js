702ab90ed51e198485b3f507617f5db82961fb3e
dong-geon-Lee <ch9716@naver.com>
2023-04-06 11:21:06 +0900

    BigData 더미데이터 제작

---
+++ src/data/mockData.js
@@ -0,0 +1,38 @@
+export const items = [
+  {
+    id: 1,
+    title: "Data Operations : Architecture & Security",
+    description: `프로젝트 실습을 통해 Data 오픈소스를 상황에 맞게 최적의 아키텍처를 설계할 수 있는 방법 및 기술을 습득`,
+    category: "BigData",
+  },
+  {
+    id: 2,
+    title: "Data Analysis : KUDU/Impala/Modern Tools",
+    description: `시간 라이브 소스에서 데이터를 스트리밍하고 위의 엔진에 데이터가 도착하는 즉시 처리 역할을 이해`,
+    category: "BigData",
+  },
+  {
+    id: 3,
+    title: "Data Processing with Spark",
+    description: `Spark를 활용한 빅데이터 분석 방법을 이해하고 실제 업무에 적용할 수 있는 방법 학습`,
+    category: "BigData",
+  },
+  {
+    id: 4,
+    title: "실습과 프로젝트로 배우는 Python 입문",
+    description: `본 과정은 실습과 프로젝트를 통해 Python의 문법, 조건문, 자료 구조, 반복문, 함수, 클래스 등을 학습합니다. 코딩 초보가 프로그래밍이 동작하는 기본적인 원리를 이해하고 재미있게 파이썬 프로그램 작성해 나갈 수 있도록 구성되어 있습니다.`,
+    category: "BigData",
+  },
+  {
+    id: 5,
+    title: "파이썬 라이브러리와 데이터 전처리",
+    description: `본 과정에서는 Python의 주요 라이브러리인 Numpy, Pandas 라이브러리를 학습하고, 원시 데이터를 처리하여 분석에 적합한 형태로 만드는 방법들을 학습합니다.`,
+    category: "BigData",
+  },
+  {
+    id: 6,
+    title: "파이썬 데이터 분석",
+    description: `데이터 분석 프로세스를 이해하고 각 단계에서 필요로 하는 기술 내용들을 학습합니다. Python 시각화 라이브러리인 matplotlib를 활용하고, 정보의 종류에 따라 시각화, 수치화를 통해 분석하는 방법들을 학습합니다.`,
+    category: "BigData",
+  },
+];
