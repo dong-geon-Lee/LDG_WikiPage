a79436d5f4da043d689c3a115588d4a011d71c8a
dong-geon-Lee <ch9716@naver.com>
2023-04-06 16:16:34 +0900

    Header 동적으로 제목 표시하기

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import * as S from "./styles";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+  const headerTitle = location.pathname === "/" ? "메인페이지" : "위키페이지";
+
   return (
     <S.Container>
       <S.Div>
-        <S.Title>WikiPage 코스 목록</S.Title>
+        <S.Title>{headerTitle}</S.Title>
         <S.Button>강의 추가하기</S.Button>
       </S.Div>
     </S.Container>
