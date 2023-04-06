00ad5d7138162bf342de547e09012870c340ed4d
dong-geon-Lee <ch9716@naver.com>
2023-04-06 11:11:20 +0900

    초기 리액트 라우터 돔 구축 및 스타일 적용

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styles";
+
+const MainPage = () => {
+  return (
+    <S.Container>
+      <h1> MainPage</h1>
+    </S.Container>
+  );
+};
+
+export default MainPage;
---
+++ src/pages/WikiPage/WikiPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styles";
+
+const WikiPage = () => {
+  return (
+    <S.Container>
+      <h1> WikiPage</h1>
+    </S.Container>
+  );
+};
+
+export default WikiPage;
